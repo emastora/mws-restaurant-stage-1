cfcd4836e5504fadbcd7a176d57f9ae0acccca42
emastora <emastora88@gmail.com>
2018-05-24 19:52:05 +0300

    First Submit

--- sw.js
+++ sw.js
@@ -1,54 +1,7 @@
 console.log("Hello Service Worker!");
 
-//Fetching events
-
-// self.addEventListener('fetch', function(event) {
-//     console.log(event);
-// });
-
-//Hello world to everything
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         new Response('Hello <b class ="a-winner-is-me"> World</b>', {
-
-//             headers: { 'Content-Type': 'text/Html' }
-
-//         })
-//     );
-// });
-
-
-//Specific image for everything
-
-// self.addEventListener('fetch', function(event) {
-//     if (event.request.url.endsWith('.jpg')) {
-//         event.respondWith(
-//             fetch('/img/offline.jpg')
-//         )
-//     }
-// });
-
-// 404 and NO SERVER responses
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         fetch(event.request).then(function(response) {
-//             if (response.status == 404) {
-//                 return new Response("Not found buddy, sorry!");
-//             }
-//             return response;
-//         }).catch(function() {
-//             return new Response('Loooooser!')
-//         })
-//     );
-// });
-
-
-// var staticCacheName = 'JohnyCache6';
-
 self.addEventListener('install', function(event) {
-    event.waituntil(
+    event.waitUntil(
         caches.open('JohnyCache4').then(function(cache) {
             return cache.addAll([
                 '/',
@@ -66,25 +19,10 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-    event.waituntil(
+    event.waitUntil(
         caches.delete('JohnyCache3')
     )
 })
-
-// self.addEventListener('install', function(event) {
-//     event.waituntil(
-//         caches.keys().then(function(staticCacheName) {
-//             return Promise.all(
-//                 cacheNames.filter(function(staticCachName) {
-//                     return cachName.startsWith('JohnyCache') &&
-//                         cacheName != staticCacheName;
-//                 }).map(function(casheName) {
-//                     return cache.delete(casheName);
-//                 })
-//             )
-//         })
-//     )
-// })
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
@@ -95,28 +33,3 @@ self.addEventListener('fetch', function(event) {
         })
     )
 })
-
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(caches.match(event.request).then(function(response) {
-//         // caches.match() always resolves
-//         // but in case of success response will have value
-//         if (response !== undefined) {
-//             return response;
-//         } else {
-//             return fetch(event.request).then(function(response) {
-//                 // response may be used only once
-//                 // we need to save clone to put one copy in cache
-//                 // and serve second one
-//                 let responseClone = response.clone();
-
-//                 caches.open('v1').then(function(cache) {
-//                     cache.put(event.request, responseClone);
-//                 });
-//                 return response;
-//             }).catch(function() {
-//                 return caches.match('/sw-test/gallery/myLittleVader.jpg');
-//             });
-//         }
-//     }));
-// });
